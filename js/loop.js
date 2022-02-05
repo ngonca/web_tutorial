c6dc42f342c92a983c9196c1f66a03d26d2d5661
Nazlı Gonca <oliviagonca@Olivia-MacBook-Air-6.local>
2022-02-05 19:29:23 +0300

    Add loop file

---
+++ js/loop.js
@@ -0,0 +1,15 @@
+
+/**
+ * For loop
+ * Döngü yaratmaya yarar. Başlangıç durumu, conditionel ifade ve son ifde diye3 parametre alır.
+ * for(başlangıç_durumu ; logic ifade ; son durum  ) {
+ *  // kodlar
+ * }
+ */
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+    
+}
+
+
