00e2e51a8dc013bdf0eecade4585a0c1b75c083c
nurullah gumus <ngumus@aefis.com>
2022-02-05 19:42:08 +0300

    Add files

--- js/loop.js
+++ js/loop.js
@@ -1,15 +1,27 @@
 
 /**
- * For loop
- * Döngü yaratmaya yarar. Başlangıç durumu, conditionel ifade ve son ifde diye3 parametre alır.
- * for(başlangıç_durumu ; logic ifade ; son durum  ) {
- *  // kodlar
- * }
+ * 
+ * for ( inital ; logic -> sürekli kontrol ediyor (her döngüde); )
+ * logic true döndüğü sürece for döner.
  */
 
-for (let i = 0; i < 10; i++) {
-    console.log(i);
+for (let index = 0; index < 20; index += 2) {
+    
+
+    console.log(index);
     
 }
+/*
+while( condition ){
+logic ifade while döngüsü çalışır
 
 
+}
+*/
+let i = 0;
+// condition True döndüpü sürece while döner
+while(i< 20){
+
+    console.log(i);
+    i++;
+}
